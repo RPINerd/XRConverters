44cb50bce47ff34e44155da1e3552ba2dc1f821c
RPINerd <RPINerd@gmail.com>
2025-09-02 20:18:10 -0400

    XRConverters source code archive

---
+++ src/XRConvertersMain/StdInc.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <stdio.h>
+#include <string>
+#include <boost/algorithm/string.hpp>
+#include <boost/filesystem.hpp>
+
+#include <Windows.h>
+
+#include "../XRConverters/dll.h"
